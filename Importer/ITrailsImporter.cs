ca3292fd7e843be37052adf4bd41d9177b672aa4
Scott Wegner <swegner2@live.com>
2013-02-25 19:57:19 -0800

    Add ITrailsImporter definition

---
+++ Importer/ITrailsImporter.cs
@@ -0,0 +1,21 @@
+﻿namespace MyTrails.Importer
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Imports trails from WTA into the MyTrails data store.
+    /// </summary>
+    public interface ITrailsImporter
+    {
+        /// <summary>
+        /// The run mode for the importer.
+        /// </summary>
+        ImportMode Mode { get; set; }
+
+        /// <summary>
+        /// Import and update trails according to the configured <see cref="Mode"/>
+        /// </summary>
+        /// <returns>Task for asynchronous completion.</returns>
+        Task Run();
+    }
+}
---
+++ Importer/ImportMode.cs
@@ -0,0 +1,31 @@
+﻿namespace MyTrails.Importer
+{
+    using System;
+
+    /// <summary>
+    /// The run mode in which to run the importer.
+    /// </summary>
+    [Flags]
+    public enum ImportMode
+    {
+        /// <summary>
+        /// Default value, don't import.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Import new trails, don't update existing.
+        /// </summary>
+        ImportOnly = 1 << 0,
+
+        /// <summary>
+        /// Update existing trails only, don't import new trails.
+        /// </summary>
+        UpdateOnly = 1 << 1,
+
+        /// <summary>
+        /// Import new trails and update existing.
+        /// </summary>
+        ImportAndUpdate = ImportOnly | UpdateOnly,
+    }
+}
