1546866ac5d153967f0d4dbce9a1382b763b9f4d
Scott Wegner <swegner2@live.com>
2013-02-25 16:48:28 -0800

    Add the Trail class

---
+++ Contracts/Data/Trail.cs
@@ -0,0 +1,23 @@
+﻿namespace MyTrails.Contracts.Data
+{
+    /// <summary>
+    /// A hiking trail.
+    /// </summary>
+    public class Trail
+    {
+        /// <summary>
+        /// Data store ID.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The WTA ID of the trail.
+        /// </summary>
+        public string WtaId { get; set; }
+
+        /// <summary>
+        /// Name of the hike.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
