f2e3b36ca81352f29a41ffe3562d7e4ec97e02dc
Scott Wegner <swegner2@live.com>
2013-02-25 19:38:15 -0800

    Add Entity Framework reference

---
+++ DataAccess/MyTrailsContext.cs
@@ -0,0 +1,17 @@
+﻿namespace MyTrails.DataAccess
+{
+    using System.Data.Entity;
+
+    using MyTrails.Contracts.Data;
+
+    /// <summary>
+    /// Data context for MyTrails data store access.
+    /// </summary>
+    public class MyTrailsContext : DbContext
+    {
+        /// <summary>
+        /// Hiking trails available in the data store.
+        /// </summary>
+        public DbSet<Trail> Trails { get; set; }
+    }
+}
