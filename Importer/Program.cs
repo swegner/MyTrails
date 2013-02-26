1df460971293977d30271a6d8f8991cfa79ff0b6
Scott Wegner <swegner2@live.com>
2013-02-25 16:57:33 -0800

    Add Importer project

---
+++ Importer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTrails.Importer
+{
+    /// <summary>
+    /// Pulls new and updated trail information from the WTA website.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Entry point to the application.
+        /// </summary>
+        /// <param name="args">Commandline arguments.</param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
