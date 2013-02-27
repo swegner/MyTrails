37d58fff1365c0b5cb9a685c180e607f9b7a967e
Scott Wegner <swegner2@live.com>
2013-02-27 15:30:07 -0800

    Enable package repository for unit tests

---
+++ Importer.Test/TestAssembly.cs
@@ -0,0 +1,26 @@
+﻿namespace Importer.Test
+{
+    using System;
+    using System.IO;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Run test assembly initilization and cleanup.
+    /// </summary>
+    [TestClass]
+    public static class TestAssembly
+    {
+        /// <summary>
+        /// Initialize test context for the assembly.
+        /// </summary>
+        /// <param name="context">The parameter is not used.</param>
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            // DataDirectory must be set for localdb context.
+            AppDomain.CurrentDomain.SetData("DataDirectory",
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, string.Empty));
+        }
+    }
+}
--- Importer.Test/TrailsImporterTests.cs
+++ Importer.Test/TrailsImporterTests.cs
@@ -4,9 +4,7 @@
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using FluentAssertions;
-
     using Importer.Test.Logging;
-
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
     using MyTrails.Importer;
