9ac48347af6f536d9271bd0e84a2194d5bfdf34a
Scott Wegner <swegner2@live.com>
2013-02-26 19:31:15 -0800

    Define WTA client interface and objects

---
+++ Importer/Wta/IWtaClient.cs
@@ -0,0 +1,17 @@
+﻿namespace MyTrails.Importer.Wta
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Interface for communicating with wta.org
+    /// </summary>
+    public interface IWtaClient
+    {
+        /// <summary>
+        /// Fetch trail definitions from WTA.
+        /// </summary>
+        /// <returns>A sequence of trails from WTA.</returns>
+        Task<IEnumerable<WtaTrail>> FetchTrails();
+    }
+}
---
+++ Importer/Wta/WtaTrail.cs
@@ -0,0 +1,9 @@
+﻿namespace MyTrails.Importer.Wta
+{
+    /// <summary>
+    /// Trail definition from the WTA website.
+    /// </summary>
+    public class WtaTrail
+    {
+    }
+}
