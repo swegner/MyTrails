56fb9d62fc07b514e8519c9aa1205a8b09d00244
Scott Wegner <swegner2@live.com>
2013-04-03 09:36:53 -0700

    Improve ImportStatus script for running executions

--- DataAccess/Scripts/ImportStatus.sql
+++ DataAccess/Scripts/ImportStatus.sql
@@ -2,9 +2,10 @@
 
 SELECT TOP 20
 	e.Id,
-	CONVERT(NVARCHAR(MAX), SWITCHOFFSET(e.StartTime, @timeZone), 100) AS StartTime,
-	CONVERT(NVARCHAR(MAX), SWITCHOFFSET(e.LastHeartbeat, @timeZone), 100) AS LastHeartbeat,
-	DATEDIFF(MINUTE, e.StartTime, e.CompletedTime) AS RunTimeMinutes,
+	 CONVERT(NVARCHAR(MAX), SWITCHOFFSET(e.StartTime, @timeZone), 100) AS StartTime,
+	 CONVERT(NVARCHAR(MAX), SWITCHOFFSET(e.LastHeartbeat, @timeZone), 100) AS LastHeartbeat,
+	CASE WHEN e.CompletedTime IS NOT NULL THEN 1 ELSE 0 END AS IsCompleted,
+	DATEDIFF(MINUTE, e.StartTime, ISNULL(e.CompletedTime, SYSDATETIMEOFFSET())) AS RunTimeMinutes,
 	e.CompletedTrailsCount - e.StartTrailsCount TrailsAdded,
 	e.CompletedTripReportsCount - e.StartTripReportsCount TripReportsAdded,
 	e.ErrorsCount,
