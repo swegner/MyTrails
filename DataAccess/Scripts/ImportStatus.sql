5910ea5183d2ab656a75a7ae53b6bd7781808aad
Scott Wegner <swegner2@live.com>
2013-04-03 09:21:27 -0700

    Add SQL script to query execution status

---
+++ DataAccess/Scripts/ImportStatus.sql
@@ -0,0 +1,14 @@
+﻿DECLARE @timeZone VARCHAR(30) = '-07:00'
+
+SELECT TOP 20
+	e.Id,
+	CONVERT(NVARCHAR(MAX), SWITCHOFFSET(e.StartTime, @timeZone), 100) AS StartTime,
+	CONVERT(NVARCHAR(MAX), SWITCHOFFSET(e.LastHeartbeat, @timeZone), 100) AS LastHeartbeat,
+	DATEDIFF(MINUTE, e.StartTime, e.CompletedTime) AS RunTimeMinutes,
+	e.CompletedTrailsCount - e.StartTrailsCount TrailsAdded,
+	e.CompletedTripReportsCount - e.StartTripReportsCount TripReportsAdded,
+	e.ErrorsCount,
+	e.ErrorString
+FROM ImportLogEntries e WITH (NOLOCK)
+ORDER BY e.Id DESC
+
